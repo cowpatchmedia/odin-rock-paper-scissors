d5abb21c92d001f65b11400b9594e1f568d2e005
cowpatchmedia <cowpatchmedia@gmail.com>
2025-10-24 19:40:19 -0700

    add 5 rounds to be played before end of RPS.

--- script.js
+++ script.js
@@ -1,34 +1,30 @@
+function getCompChoice () { //this randomly generates a computer choice based on random and percentages
+        let randNum = Math.random();
+        let compResult = "";
 
-let humanScore = 0;
-let compScore = 0;
+        if (randNum <= .33) {
+            compResult = "Rock"
+        } else if (randNum <= .67) {
+            compResult = "Paper"
+        } else {
+            compResult = "Scissors"
+        }
+        return compResult; //return result so that function can be called in other functions
+    };
 
-function getCompChoice () {
-    let randNum = Math.random();
-    let compResult = "";
-    
-    if (randNum <= .33) {
-        compResult = "Rock"
-    } else if (randNum <= .67) {
-        compResult = "Paper"
-    } else {
-        compResult = "Scissors"
-    }
-    return compResult;
-};
-
-function getHumanChoice () {
+function getHumanChoice () { //let human enter in answer
     let humanResult = prompt("Enter Rock, Paper, or Scissors");
-    return humanResult;
+    return humanResult; // return result so it can be referenced in other functions
 };
 
-function playRound(compChoice,humanChoice){
+function playRound(compChoice,humanChoice){ //explicitly check for human win conditions, if not then computer wins round
     if (humanChoice === compChoice){
-        console.log("It's a tie.");
+        console.log("It's a tie."); // tie don't increment scores
     }
     else if (
         (humanChoice === "Scissors" && compChoice === "Paper") || //double line is "OR"
-        (humanChoice === "Rock" && compChoice === "Scissor") ||
-        (humanChoice === "Paper" && compChoice === "Scissor")
+        (humanChoice === "Rock" && compChoice === "Scissors") || // and is using &&
+        (humanChoice === "Paper" && compChoice === "Rock") // use === not = which changes value
         ){
             console.log(`${humanChoice} beats ${compChoice}! You win this round.`);// ${} prints variable
             humanScore++;
@@ -37,15 +33,21 @@ function playRound(compChoice,humanChoice){
         console.log(`${compChoice} beats ${humanChoice}! You lose this round.`);// make sure to use backtick ` not '
         compScore++;    
     }
+    console.log("Human Score:", humanScore); //print scores after round is done and win message is displayed
+    console.log("Computer Score:", compScore);
 };
 
 function playGame(){
-    playRound(compSelection, humanSelection);
+
+    playRound(getCompChoice(), getHumanChoice()); // doing 5 rounds like this since I don't know loops yet. 
+    playRound(getCompChoice(), getHumanChoice());
+    playRound(getCompChoice(), getHumanChoice());
+    playRound(getCompChoice(), getHumanChoice());
+    playRound(getCompChoice(), getHumanChoice());
+
+    console.log("Final Score: Human", humanScore, "| ", "Computer:", compScore) //prints final score at end of game.
 };
 
-let humanSelection = getHumanChoice();
-let compSelection = getCompChoice();
-
-playGame();
-console.log("Human Score:", humanScore);
-console.log("Computer Score:", compScore);
+let humanScore = 0; //initializes score variables
+let compScore = 0;
+playGame(); // calls playRound 5x 
